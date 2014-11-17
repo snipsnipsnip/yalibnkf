0a92f64fe00b07ab1ee7db71fe1b6a3671382787
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2014-11-17 23:00:13 +0900

    Replace python api with plain stdlib

--- yalibnkf.c
+++ yalibnkf.c
@@ -1,10 +1,14 @@
 /*
+  yalibnkf.c
+  based on Python Interface to NKF
+*/
+/*
 Changes.
 2009.6.2    Remove WISH_TRUE, use get_guessed_code() for nkf-2.0.9
                  by SATOH Fumiyasu (fumiyas @ osstech co jp)
 2008.7.17   Change the type of strlen from long to int, by SATOH Fumiyasu.
 2007.2.1    Add guess() function.
-2007.1.13   Remove pynkf_parseopts(), by SATOH Fumiyasu.
+2007.1.13   Remove yalibnkf_parseopts(), by SATOH Fumiyasu.
 */
 /**  Python Interface to NKF
 ***************************************************************************
@@ -20,183 +24,129 @@ Changes.
 **    THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE.
 ***************************************************************************/
 
-#include "Python.h"
 #include <setjmp.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #undef getc
 #undef ungetc
-#define getc(f)         pynkf_getc(f)         
-#define ungetc(c,f)     pynkf_ungetc(c,f)
+#define getc(f)         yalibnkf_getc(f)         
+#define ungetc(c,f)     yalibnkf_ungetc(c,f)
 
 #undef putchar
 #undef TRUE
 #undef FALSE
-#define putchar(c)      pynkf_putchar(c)
+#define putchar(c)      yalibnkf_putchar(c)
 
-static int pynkf_ibufsize, pynkf_obufsize;
-static unsigned char *pynkf_inbuf, *pynkf_outbuf;
-static int pynkf_icount,pynkf_ocount;
-static unsigned char *pynkf_iptr, *pynkf_optr;
+static int yalibnkf_ibufsize, yalibnkf_obufsize;
+static unsigned char *yalibnkf_inbuf, *yalibnkf_outbuf;
+static int yalibnkf_icount,yalibnkf_ocount;
+static unsigned char *yalibnkf_iptr, *yalibnkf_optr;
 static jmp_buf env;
-static int pynkf_guess_flag;
+static int yalibnkf_guess_flag;
 
 static int 
-pynkf_getc(FILE *f)
+yalibnkf_getc(FILE *f)
 {
   unsigned char c;
-  if (pynkf_icount >= pynkf_ibufsize) return EOF;
-  c = *pynkf_iptr++;
-  pynkf_icount++;
+  if (yalibnkf_icount >= yalibnkf_ibufsize) return EOF;
+  c = *yalibnkf_iptr++;
+  yalibnkf_icount++;
   return (int)c;
 }
 
 static int 
-pynkf_ungetc(int c, FILE *f)
+yalibnkf_ungetc(int c, FILE *f)
 {
-  if (pynkf_icount--){
-    *(--pynkf_iptr) = c;
+  if (yalibnkf_icount--){
+    *(--yalibnkf_iptr) = c;
     return c;
   }else{ return EOF; }
 }
 
 static void
-pynkf_putchar(int c)
+yalibnkf_putchar(int c)
 {
   size_t size;
   unsigned char *p;
 
-  if (pynkf_guess_flag) {
+  if (yalibnkf_guess_flag) {
     return;
   }
 
-  if (pynkf_ocount--){
-    *pynkf_optr++ = c;
+  if (yalibnkf_ocount--){
+    *yalibnkf_optr++ = c;
   }else{
-    size = pynkf_obufsize + pynkf_obufsize;
-    p = (unsigned char *)PyMem_Realloc(pynkf_outbuf, size + 1);
-    if (pynkf_outbuf == NULL){ longjmp(env, 1); }
-    pynkf_outbuf = p;
-    pynkf_optr = pynkf_outbuf + pynkf_obufsize;
-    pynkf_ocount = pynkf_obufsize;
-    pynkf_obufsize = size;
-    *pynkf_optr++ = c;
-    pynkf_ocount--;
+    size = yalibnkf_obufsize + yalibnkf_obufsize;
+    p = (unsigned char *)realloc(yalibnkf_outbuf, size + 1);
+    if (yalibnkf_outbuf == NULL){ longjmp(env, 1); }
+    yalibnkf_outbuf = p;
+    yalibnkf_optr = yalibnkf_outbuf + yalibnkf_obufsize;
+    yalibnkf_ocount = yalibnkf_obufsize;
+    yalibnkf_obufsize = size;
+    *yalibnkf_optr++ = c;
+    yalibnkf_ocount--;
   }
 }
 
 #define PERL_XS 1
-#include "../utf8tbl.c"
-#include "../nkf.c"
+#include "nkf/utf8tbl.c"
+#include "nkf/nkf.c"
 
-static PyObject *
-pynkf_convert(unsigned char* str, int strlen, char* opts, int optslen)
+const char *
+yalibnkf_convert(unsigned char* str, int strlen, char* opts, int optslen)
 {
-  PyObject * res;
+  const char *ret;
 
-  pynkf_ibufsize = strlen + 1;
-  pynkf_obufsize = pynkf_ibufsize * 1.5 + 256;
-  pynkf_outbuf = (unsigned char *)PyMem_Malloc(pynkf_obufsize);
-  if (pynkf_outbuf == NULL){
-    PyErr_NoMemory();
+  if (yalibnkf_outbuf != NULL) {
     return NULL;
   }
-  pynkf_outbuf[0] = '\0';
-  pynkf_ocount = pynkf_obufsize;
-  pynkf_optr = pynkf_outbuf;
-  pynkf_icount = 0;
-  pynkf_inbuf  = str;
-  pynkf_iptr = pynkf_inbuf;
-  pynkf_guess_flag = 0;
+
+  yalibnkf_ibufsize = strlen + 1;
+  yalibnkf_obufsize = yalibnkf_ibufsize * 1.5 + 256;
+  yalibnkf_outbuf = (unsigned char *)malloc(yalibnkf_obufsize);
+  if (yalibnkf_outbuf == NULL){
+    return NULL;
+  }
+  yalibnkf_outbuf[0] = '\0';
+  yalibnkf_ocount = yalibnkf_obufsize;
+  yalibnkf_optr = yalibnkf_outbuf;
+  yalibnkf_icount = 0;
+  yalibnkf_inbuf  = str;
+  yalibnkf_iptr = yalibnkf_inbuf;
+  yalibnkf_guess_flag = 0;
 
   if (setjmp(env) == 0){
-
     reinit();
-
     options(opts);
 
     kanji_convert(NULL);
 
   }else{
-    PyMem_Free(pynkf_outbuf);
-    PyErr_NoMemory();
+    free(yalibnkf_outbuf);
     return NULL;
   }
 
-  *pynkf_optr = 0;
-  res = PyString_FromString(pynkf_outbuf);
-  PyMem_Free(pynkf_outbuf);
-  return res;
+  *yalibnkf_optr = 0;
+  ret = yalibnkf_outbuf;
+  yalibnkf_outbuf = NULL;
+
+  return ret;
 }
 
-static PyObject *
-pynkf_convert_guess(unsigned char* str, int strlen)
+const char *
+yalibnkf_convert_guess(unsigned char* str, int strlen)
 {
-  PyObject * res;
-  const char *codename;
+  yalibnkf_ibufsize = strlen + 1;
+  yalibnkf_icount = 0;
+  yalibnkf_inbuf  = str;
+  yalibnkf_iptr = yalibnkf_inbuf;
 
-  pynkf_ibufsize = strlen + 1;
-  pynkf_icount = 0;
-  pynkf_inbuf  = str;
-  pynkf_iptr = pynkf_inbuf;
-
-  pynkf_guess_flag = 1;
+  yalibnkf_guess_flag = 1;
   reinit();
   guess_f = 1;
 
   kanji_convert(NULL);
 
-  codename = get_guessed_code();
-
-  res = PyString_FromString(codename);
-  return res;
+  return get_guessed_code();
 }
-
-#ifndef EXTERN_NKF
-static
-#endif
-PyObject *pynkf_nkf(PyObject *self, PyObject *args)
-{
-  unsigned char *str;
-  int strlen;
-  char *opts;
-  int optslen;
-  PyObject* res;
-
-  if (!PyArg_ParseTuple(args, "s#s#", &opts, &optslen, &str, &strlen)) {
-    return NULL;
-  }
-  res = pynkf_convert(str, strlen, opts, optslen);
-  return res;
-}
-
-#ifndef EXTERN_NKF
-static
-#endif
-PyObject *pynkf_guess(PyObject *self, PyObject *args)
-{
-  unsigned char *str;
-  int strlen;
-  PyObject* res;
-
-  if (!PyArg_ParseTuple(args, "s#", &str, &strlen)) {
-    return NULL;
-  }
-  res = pynkf_convert_guess(str, strlen);
-  return res;
-}
-
-#ifndef EXTERN_NKF
-static PyMethodDef
-nkfmethods[] = {
-  {"nkf", pynkf_nkf, METH_VARARGS},
-  {"guess", pynkf_guess, METH_VARARGS},
-  {NULL, NULL}
-};
-
-/* Module initialization function */
-void
-initnkf(void)
-{
-  Py_InitModule("nkf", nkfmethods);
-}
-#endif
