11172a29f972c21ef55b02d92965bd284f1342c0
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2015-01-16 01:07:58 +0900

    All tests passed :tea:
    Turns out that 18 errors was caused by nkf's options().
    To avoid buggy behavior, pass options one by one after split by spaces.

--- tools/convert_nkf_test.pl
+++ tools/convert_nkf_test.pl
@@ -10,26 +10,26 @@ my $DESCRIPTION = <<'EOS';
   Implement following functions:
 
     void description(const char *message);
+    void test_input(const char *nkf_options, int input_length, const char *input);
+    void test_output(int answer_length, const char *answer);
 
-    void testcase(const char *nkf_options,
-                  int input_length, const char *input,
-                  int answer_length, const char *answer,
-                  int num_alternative_answers, ...);
-
-  testcase() must allow variable length arguments.
-  Number of arguments is 7 + num_alternative_answers * 2.
-  num_alternative_answers is followed by the length-string pairs repeating num_alternative_answers times.
-  String lengths are important since string data except nkf_options often
+  test_input() is followed by one or more calls of test_output().
+  String lengths are important since string data, except nkf_options, often
   contain one or more non-terminating '\0's.
 
   Example:
 
     description("`nkf -foo` must convert 'bar' into either 'hoge' or 'fuga'.");
-    testcase("-foo", 3, "bar", 4, "hoge", 1, 4, "fuga");
+    test_input("-foo", 3, "bar");
+    test_output(4, "hoge");
+    test_output(4, "fuga");
     description("`nkf -baz` must convert 'moga' into 'foge'.");
-    testcase("-baz", 3, "moga", 4, "foge", 0);
+    test_input("-baz", 3, "moga");
+    test_output(4, "foge");
 
-  Note: In original test.pl test runner, special treatmentspace letters (' ') in output and answers are ignored if $nkf_options matches regexp /-\w+m[NS]/.
+  Note: In original test.pl test runner, there are special treatments
+  if $nkf_options matches regexp /-\w+m[NS]/, in which case
+  space letters (' ') in output and answers are ignored.
 */
 EOS
 
@@ -61,12 +61,12 @@ sub translate_case {
   my $arg = quote(escape(strip($head)));
 
   if ($name =~ /\Atestcase\z/) {
-    my ($input, $answer, @alternative_answers) = map { length($_) . ', ' . quote(escape($_)); } @args;
+    my ($input, @answers) = map { length($_) . ', ' . quote(escape($_)); } @args;
 
-    $arg .= join(', ', '', $input, $answer, scalar(@alternative_answers), @alternative_answers);
+    join "\n", map { "$_->[0]($_->[1]);" } [test_input => "$arg, $input"], map { [test_output => $_] } @answers;
+  } else {
+    "$name($arg);";
   }
-
-  "$name($arg);";
 }
 
 sub quote {
@@ -74,15 +74,15 @@ sub quote {
 }
 
 sub escape {
-  my ($_) = @_;
-  s/[^\n\r0-9a-zA-Z]/sprintf '\%03o', ord $&/gse;
+  local ($_) = @_;
+  s/[^- \(\)\/_\[\]:=\n\ra-zA-Z]/sprintf '\%03o', ord $&/gse;
   s/\n/\\n/g;
   s/\r/\\r/g;
   $_;
 }
 
 sub strip {
-  my ($_) = @_;
+  local ($_) = @_;
   s/\A\s+|\s+\z//;
   $_;
 }
--- tools/pipe_test.pl
+++ tools/pipe_test.pl
@@ -10,10 +10,10 @@ main() if __FILE__ eq $0;
 sub main {
   my $yankf = $ARGV[0];
   my $test_cases = $ARGV[1] or die "usage: $0 yankf.exe test_cases.inc\n\nRuns nkf test, generated from convert_nkf_test.pl, using pipelines.\n";
-  
+
   my @cases = load_test_cases($test_cases);
   my $error_count;
-  
+
   for (@cases) {
     if (run_test($yankf, $_)) {
       print "Ok\n";
@@ -22,7 +22,7 @@ sub main {
       $error_count++;
     }
   }
-  
+
   if ($error_count > 1) {
     print $error_count, " errors were found.\n";
   } elsif ($error_count == 1) {
@@ -30,61 +30,61 @@ sub main {
   } else {
     print "All tests are succeeded.\n";
   }
-  
+
   exit 1 if $error_count > 0;
 }
 
 sub load_test_cases {
   my $src = read_file($_[0]);
-  
+
   $src =~ s/.*\*\///s;
   $src =~ s/[$@%]/\\\0/g;
-  $src =~ s/^([a-z]+)\(/\$$1->(/gm;
-  
+  $src =~ s/^([a-z_]+)\(/\$$1->(/gm;
+
   my @cases;
   my @descs;
-  
+
   my $description = sub {
     push @descs, @_;
   };
 
-  my $testcase = sub {
+  my $test_input = sub {
     if (@descs) {
       push @cases, {description => [@descs], tests => []};
       @descs = ();
     }
-    my ($options, $input, @answers) = omit_lengthes(@_);
-    push @{$cases[$#cases]->{tests}}, {options => $options, input => $input, answers => [@answers]};
+    my ($options, undef, $input) = @_;
+    push @{$cases[$#cases]->{tests}}, {options => $options, input => $input, answers => []};
   };
-  
+
+  my $test_output = sub {
+    my (undef, $answer) = @_;
+    push @{$cases[$#cases]->{tests}->[-1]->{answers}}, $answer;
+  };
+
   eval $src;
   die "Failed loading test cases: $@" if $@;
   die "Test case format error: dangling descriptions" if @descs;
-  
-  @cases;
-}
 
-sub omit_lengthes {
-  6 + $_[5] * 2 == @_ or die "Test case format error: wrong number of arguments for testcase()";
-  @_[0, 2, 4, map { 5 + 2 * $_ } 1..$_[5]];
+  @cases;
 }
 
 sub run_test {
   my ($yankf, $bundle) = @_;
 
   printf "%-40s", $_ for @{$bundle->{description}};
-  
+
   for my $case (@{$bundle->{tests}}) {
     my $tolerant = $case->{options} =~ /-\w+m[NS]/;
     my ($out, $err) = run_nkf($yankf, $case->{options}, $case->{input});
-    
+
     warn "nkf warned: $err\n" if $err;
-    
+
     unless (equal($tolerant, $case->{answers}, $out)) {
       return 0;
     }
   }
-  
+
   return 1;
 }
 
@@ -96,11 +96,11 @@ sub equal {
     $actual =~ s/ //g;
     s/ //g for @answers;
   }
-  
+
   for (@answers) {
     return 1 if $actual eq $_;
   }
-  
+
   0;
 }
 
